26b6153bbbd3722bfffb961ccd673d80481c54cd
timen678 <timper1999@gmail.com>
2025-11-21 20:55:11 +0100

    Little start to website

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = "/static")
 
 @app.route("/")
 def index():
     products = [
-        {"id": 1, "name": "Computer part 1"},
-        {"id": 2, "name": "Computer part 2"}
+        {"id": 1, "name": "CPU", "price": 100, "img": "cpu"},
+        {"id": 2, "name": "RAM", "price": 50, "img": "ram"}
     ]
     return render_template("index.html", products = products)
